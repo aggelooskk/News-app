8d912313dacf79276fd2481c344fdb48a50b87ae
Aggelosk <akermezo@yahoo.com>
2024-12-19 17:52:44 +0200

    creating routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
 import { Provider } from "react-redux";
 import store from "./store";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );

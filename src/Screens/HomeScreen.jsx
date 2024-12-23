da962ee8783bd1b050ee678d99d76b921ea88391
Aggelosk <akermezo@yahoo.com>
2024-12-23 17:08:24 +0200

    error loading homescreen

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -6,15 +6,19 @@ import { Spinner } from "react-bootstrap";
 const HomeScreen = () => {
   const { data, isError, isLoading } = useGetNewsQuery();
 
- if (isLoading) return <div className="text-center my-5">Loading...</div>;
-  if (isError) return <div className="text-center my-5">An error occurred!</div>;
-
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : isError ? (
+    isError
+  ) : (
     <div className="container mt-4">
       <h1 className="text-center mb-4 fw-bold text-dark">Latest News</h1>
       <div className="row justify-content-center">
         {data?.articles?.map((article, index) => (
-          <div key={index} className="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+          <div
+            key={index}
+            className="col-12 col-md-6 col-lg-4 d-flex justify-content-center"
+          >
             <Card
               image={article.urlToImage}
               title={article.title}

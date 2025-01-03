d694c9d1b4e4f4a4895ea2e15dcb5b10bc7f3ae2
Aggelosk <akermezo@yahoo.com>
2025-01-03 16:58:48 +0200

    Card fixes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Card = ({ image, title, url, publishedAt }) => {
   return (
     <Link to={url}>
-      <div className="h-96 w-72 my-3 shadow-lg rounded-lg overflow-hidden">
+      <div className="h-96 w-72 my-3 overflow-hidden">
         <img src={image} alt={title} className="w-full h-52 object-cover" />
         <div className="p-4">
           <h5 className="text-lg font-bold">{title}</h5>

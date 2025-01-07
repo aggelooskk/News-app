3859b2781b0c071c26db98cb86f6f851f9905ef1
Aggelosk <akermezo@yahoo.com>
2025-01-07 18:35:50 +0200

    card improvments

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 
 const Card = ({ image, title, url, author }) => {
   return (
-    <Link to={url}>
-      <div className="h-73 w-72 my-3 overflow-hidden rounded-lg">
-        <img src={image} alt={title} className="w-full h-52 object-cover" />
+    <>
+      <div className="h-73 w-72 my-3 overflow-hidden rounded-lg hover:scale-105">
+        <Link to={url}>
+          <img src={image} alt={title} className="w-full h-52 object-cover" />
+        </Link>
         <div className="p-1 mt-2">
-          <h5 className="text-lg font-bold line-clamp-3">{title}</h5>
+          <Link to={url}>
+            <h5 className="text-lg font-bold line-clamp-3 hover:text-blue-600">
+              {title}
+            </h5>
+          </Link>
           <p className="text-sm text-gray-500">{author}</p>
         </div>
       </div>
-    </Link>
+    </>
   );
 };
 

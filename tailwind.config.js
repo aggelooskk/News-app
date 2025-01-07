cc5597826757bcdb4b5a8d048f3c04ba4094f04d
Aggelosk <akermezo@yahoo.com>
2025-01-07 18:16:56 +0200

    adding font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      serif: [`ui-serif`, `Georgia`],
+    },
     extend: {},
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
-
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
